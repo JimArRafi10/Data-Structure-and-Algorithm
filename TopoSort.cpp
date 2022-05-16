a2d974c1f8d1f511d2bc7b14b9aeb12646de2555
Jim Ar Rafi <70357675+JimArRafi10@users.noreply.github.com>
2022-05-16 17:43:24 +0600

    Create TopoSort.cpp

---
+++ TopoSort.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n,m;
+  cin>>n>>m;
+  int cnt=0;
+  vector<vector<int>> adj(n);
+  vector<int> indeg(n);
+  for(int i=0;i<m;i++){
+    int u,v; cin>>u>>v;
+    adj[u].push_back(v);
+    indeg[v]++;
+
+  }
+  queue<int> pq;
+  for(int i=0;i<n;i++){
+    if(indeg[i]==0){
+        pq.push(i);
+    }
+  }
+
+  while(!pq.empty()){
+    cnt++;
+    int x=pq.front();
+    pq.pop();
+    cout<<x<<" ";
+
+    for(auto it: adj[x]){
+        indeg[it]--;
+        if(indeg[it]==0)
+            pq.push(it);
+
+    }
+  }
+
+
+
+}
+
+//4 3
+// 0 1
+// 1 2
+// 2 3
+//output: 0 1 2 3
