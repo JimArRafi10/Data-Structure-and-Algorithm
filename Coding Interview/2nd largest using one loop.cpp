8ec41f09c3dde1a7f4c6d707eedf8b96e0decfb4
Jim Ar Rafi <70357675+JimArRafi10@users.noreply.github.com>
2022-07-01 19:06:17 +0600

    Update 2nd largest using one loop.cpp

--- Coding Interview/2nd largest using one loop.cpp
+++ Coding Interview/2nd largest using one loop.cpp
@@ -6,12 +6,12 @@ int  main()
    int n, large;
    cin>>n;
    int a[n];
-   int mx=0;
+
    for(int i=0; i<n; i++){
     cin>>a[i];
    }
    sort(a,a+n);
-   int slarge=0;
+   
    for(int i=n-1; i>=0; i--){
       if(a[i] != a[n-1]){
           cout<<a[i]<<endl;
